f58d6f4e55dce8f774e44cec6161a7ee81ddf3c2
Ryan Johnsen <rdjsen@gmail.com>
2020-12-27 16:52:13 -0600

    Reworked reso class so input is external

--- class_def.py
+++ class_def.py
@@ -22,10 +22,94 @@ class resolutions:
     def status(self):
         """Print Status of Resolutions"""
         
-        print(self.df.drop('type', axis=1))
+        return(self.df.drop('type', axis=1))
+        
+    def update(self, num, add):
+        """Increase reso <num> by <add>"""
+        
+        self.df.loc[num, 'current'] = self.df.loc[num,'current'] + add
+        
+        cur_type = cur_type = self.df.loc[num,'type']
+        
+        if (cur_type == 'One-Off') and (self.df.loc[num, 'current'] >= self.df.loc[num, 'target']):
+            #if a oneoff is completed
+            self.df.loc[num, 'percent'] = 'Complete'
+        
+        #saving
+        self.update_percentage()
+        self._save_csv()
+        
+            
+                
+    def add(self, reso, res_type, goal, current):
+        """Add a New Resolution"""
+        
+        if res_type == 'Yearly Total':
+            self.df.loc[len(self.df)+1] = [reso, res_type, goal, current, '0 %', 0, 0]
+        elif current < goal:
+            self.df.loc[len(self.df)+1] = [reso, res_type, goal, current, 'Incomplete', 0, 0]
+        else:
+            self.df.loc[len(self.df)+1] = [reso, res_type, goal, current, 'Complete', 0, 0]
+        
+        self.update_percentage()
+        self._save_csv()
+        
+    def delete(self, num):
+        """Remove reso <num>"""
+        
+        self.df.drop(num, inplace=True)
+        self.df.reset_index(inplace=True)
+        
+        #loop to fix the numbers
+        for i in range(0, len(self.df)):
+            self.df.loc[i, 'num'] = i + 1
+        
+        self.df.set_index('num', inplace=True)
+        self._save_csv()
     
-    def update(self):
-        """Loop through Resolutions and update"""
+    def update_percentage(self):
+        """Recalculate Resolution Percentages"""
+        
+        #code for yearcount
+        #calculate percent
+        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = (
+                self.df['current'] / self.df['target']*100)
+        
+        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = (
+                self.df.loc[self.df['type'] == 'Yearly Total', 'percent'].apply(round))
+        
+        #convert to string and add % sign
+        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = self.df['percent'].apply(str)
+        
+        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = self.df['percent'] + ' %'
+        
+        
+        #percent for one offs
+        
+        
+        #calculate pace
+        cur_week = datetime.date.isocalendar(datetime.date.today())[1]
+        self.df['pace'] = self.df['target']*(cur_week-1)/52
+        self.df['pace'] = self.df['pace'].apply(round)
+        
+        #pace percent
+        self.df['pace percent'] = str(round((cur_week-1)/52*100)) + ' %'
+        
+        
+    def _save_csv(self):
+        """Function to save the dataframe to a csv"""
+        self.df.to_csv(self.file)
+        
+    #iteration
+    def resos_list(self):
+        """Generator for the resolutions"""
+        for i in range(1, len(self.df)+1):
+            yield self.df.loc[i]
+   
+         
+# # # ~ ~ ~ DEPRECIATED CODE ~ ~ ~ # # #
+def old_update(self):
+        """Loop through Resolutions and update - will be removed"""
         
         cur_week = datetime.date.isocalendar(datetime.date.today())[1]
         
@@ -59,9 +143,22 @@ class resolutions:
         print('\n ~ ~ ~ * * * Update Complete * * * ~ ~ ~ \n' )
         self.update_percentage()
         self.status()
-        self._save_csv()        
-                
-    def add(self, display=True):
+        self._save_csv()       
+
+def old_delete(self, num):
+        """Remove a resolution - will be deleted"""
+        self.status()
+        num = int(input('\n Which resolution would you like to remove? (Number)\n'))
+        
+        print(self.df.drop(num, axis=0, inplace = False))
+    
+        correct = int(input('\nEnter 1 to save changes, or 2 to discard\n'))
+        
+        if correct == 1:
+            self.df.drop(num, axis=0, inplace = True)
+                 
+
+def old_add(self, display=True):
         """Add a New Resolution"""
         reso = input('What is the resolution to add? \n')
         res_type_int = input('\n What is the resolution type? 1 for a One-Off, 2 for Yearly Total \n' )
@@ -88,50 +185,5 @@ class resolutions:
         
         
         if display == True:
-            self.status()      
-        #TODO Save function
-        
-    def delete(self, num):
-        """Remove a resolution"""
-        self.status()
-        num = int(input('\n Which resolution would you like to remove? (Number)\n'))
-        
-        print(self.df.drop(num, axis=0, inplace = False))
-    
-        correct = int(input('\nEnter 1 to save changes, or 2 to discard\n'))
-        
-        if correct == 1:
-            self.df.drop(num, axis=0, inplace = True)
-            
-        #TODO update the resolution numbers
-    
-    def update_percentage(self):
-        """Recalculate Resolution Percentages"""
-        
-        #code for yearcount
-        #calculate percent
-        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = (
-                self.df['current'] / self.df['target']*100)
-        
-        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = (
-                self.df.loc[self.df['type'] == 'Yearly Total', 'percent'].apply(round))
-        
-        #convert to string and add % sign
-        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = self.df['percent'].apply(str)
-        
-        self.df.loc[self.df['type'] == 'Yearly Total', 'percent'] = self.df['percent'] + ' %'
-        
-        #calculate pace
-        cur_week = datetime.date.isocalendar(datetime.date.today())[1]
-        self.df['pace'] = self.df['target']*(cur_week-1)/52
-        self.df['pace'] = self.df['pace'].apply(round)
-        
-        #pace percent
-        self.df['pace percent'] = str(round((cur_week-1)/52*100)) + ' %'
-        
-        #TODO code to add the yearly percent
-    def _save_csv(self):
-        """Function to save the dataframe to a csv"""
-        self.df.to_csv(self.file)
-        
+            self.status()            
         
