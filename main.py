0a6636114ef5ffa748859ac83045fe45f0dfe78f
Ryan Johnsen <rdjsen@gmail.com>
2021-01-09 01:21:00 -0600

    Implemented Adding.  Working on percentages

--- main.py
+++ main.py
@@ -2,28 +2,29 @@
 """
 Created on Wed Dec 23 15:56:14 2020
 
-@author: rdjse
+@author: Ryan Johnsen rdjsen@gmail.com
 """
 
 #wrapper for resolution tracker
 
 import pandas as pd
-from class_def import resolutions
 from os import path
 from os import getcwd
 import telegram
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import sqlite3
+import datetime
 
-FILE_TEMPLATE = ['user_id', 'num', 'resolution', 'type', 'target', 'current', 'percent', 
-                 'pace', 'pace percent']
+FILE_TEMPLATE = ['user_id', 'num', 'resolution', 'type', 'target', 'current', 'cur_percent', 
+                 'pace', 'pace_percent']
+
+actions = ['adding', 'updating', 'deleting']
 
 conn = sqlite3.connect('data.db')
 
 #file location
 
-test = resolutions(path.join(getcwd(), "ryan.csv"))
+#test = resolutions(path.join(getcwd(), "ryan.csv"))
 
 
 def get_token():
@@ -43,6 +44,7 @@ def start(update, context):
 
 def help_command(update, context):
     """Display available commands"""
+    
     pass
 
 def start_tracking(update, context):
@@ -54,21 +56,6 @@ def start_tracking(update, context):
     #add to user_data db
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    
-    
-# # # ~ ~ ~ Deprecated Code ~ ~ ~ # # #    
-#    if path.isfile(str(user_id)+'.csv'):
-#        #check for an overrite argument?  -r?
-#        context.bot.send_message(chat_id=update.effective_chat.id,
-#                             text = "It looks like I am already tracking resolutions"
-#                             " for you.")
-#        return
-#    
-#    #create blank file
-#    blank_file = open(str(user_id)+'.csv', 'w')
-#    blank_file.write(FILE_TEMPLATE)
-#    blank_file.close()
-# # # ~ ~ ~ End Deprecated Code ~ ~ ~ # # #
     
     #removing any old resolutions
     c.execute("""DELETE FROM resolutions
@@ -105,7 +92,39 @@ def start_tracking(update, context):
 def track(update, context):
     """Response to the track command"""
     
+    #get user_id
+    user_id = update.effective_chat.id
+    
+    #db connection
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    
+    #get status
+    c.execute("""
+              SELECT status
+              FROM user_status
+              WHERE chat_id=?
+              """, (user_id,))
+    cur_status = c.fetchone()[0]
+    
+    #verify idling
+    
+    if cur_status == 'idle':
+        #give update
+        df = pd.read_sql("""
+                         SELECT *
+                         FROM resolutions
+                         WHERE user_id =?
+                         """, conn, params = (user_id,))
+        
+        #loop to give the update on each one
+    else:
+        #error message
+        pass
+    
     #TODO verify that user isn't adding, deleting, or updating
+    
+    
     #psuedocode
     #check if file exists for username
     #if not, create one
@@ -160,17 +179,17 @@ def input_loop(update, context):
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
     c.execute("""
-              SELECT status
+              SELECT status, num
               FROM user_status
               WHERE chat_id=?
               """, (user_id,))
+    
     dbpull = c.fetchone()
     cur_status = dbpull[0]
     cur_num = dbpull[1]
     
     if cur_status == 'adding':
         
-        #TODO custom keyboard input?
         if cur_num == 0:
             #Prompting type
             try:
@@ -194,10 +213,10 @@ def input_loop(update, context):
                 
                 #update status
                 c.execute("""
-                  UPDATE user_status
-                  SET num = 1
-                  WHERE chat_id=?
-                  """, (user_id,))
+                          UPDATE user_status
+                          SET num = 1
+                          WHERE chat_id=?
+                          """, (user_id,))
                 
                 conn.commit()
                 
@@ -205,25 +224,185 @@ def input_loop(update, context):
                 context.bot.send_message(chat_id=update.effective_chat.id,
                              text = "Great!  What is the resolution?")
                 
-            except (ValueError, IndexError):
+                conn.close()
+            except (ValueError, IndexError, KeyError):
                 #illegal value entered
                 context.bot.send_message(chat_id=update.effective_chat.id,
                              text = "Uh-Oh!  Illegal value entered! "
                              "Please try again!")
+                
+                conn.close()
         
         elif cur_num == 1:
-            #prompting description
-            pass
+        #prompting description
+            description = update.message.text
+            
+            #calc new_num
+            c.execute('''
+                      SELECT COUNT(*) 
+                      FROM resolutions 
+                      WHERE user_id=?
+                      ''', (user_id,))
+                
+            new_num = c.fetchone()[0]
+            
+            #add description to db
+            c.execute("""
+                      UPDATE resolutions
+                      SET resolution =?
+                      WHERE user_id=? 
+                      AND num = ?                      
+                      """, (description, user_id, new_num))
+                
+            conn.commit()
+            
+            #update status - check if yearly or one-off
+            
+            #get cur_type
+            c.execute('''
+                      SELECT type 
+                      FROM resolutions 
+                      WHERE user_id=?
+                      AND num =?
+                      ''', (user_id, new_num))
+                
+            cur_type = c.fetchone()[0]
+            
+            if cur_type == 'Yearly Total':
+            
+                #if yearly total, move on to prompting for goal and current
+                c.execute("""
+                          UPDATE user_status
+                          SET num = 2
+                          WHERE chat_id=?
+                          """, (user_id,))
+                
+                conn.commit()
+                conn.close()
+                
+                #response message
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                             text = "Perfect!  What is your goal for the year? (Enter a number)")
+                
+            else:
+                #type is one off - end adding
+                
+                #update status
+                c.execute("""
+                          UPDATE user_status
+                          SET num = 0,
+                          status = 'idle'
+                          WHERE chat_id=?
+                          """, (user_id,))
+               
+                conn.commit()
+                conn.close() 
+                
+                #response message
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                             text = "Perfect!  The Resolution has been added!")
+                
+                update_percents(user_id)
     
         elif cur_num == 2:
             #prompting goal if type is yearly
-            pass
+            try:
+                cur_goal = int(update.message.text)
+                
+                if cur_goal <= 0:
+                    #illegal input, but doesn't raise error
+                    raise ValueError
+                    
+                #calc newnum
+                c.execute('''
+                          SELECT COUNT(*) 
+                          FROM resolutions 
+                          WHERE user_id=?
+                          ''', (user_id,))
+                
+                new_num = c.fetchone()[0]
+                
+                #update db
+                c.execute("""
+                          UPDATE resolutions
+                          SET target =?
+                          WHERE user_id=? 
+                          AND num = ?                      
+                          """, (cur_goal, user_id, new_num))
+                
+                conn.commit()
+                
+                #update status
+                c.execute("""
+                          UPDATE user_status
+                          SET num = 3
+                          WHERE chat_id=?
+                          """, (user_id,))
+                
+                conn.commit()
+                conn.close()
+                
+                #send response
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text = "Fantastic!  How many have you completed?")
+                
+                
+            except ValueError:
+                #illegal input
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text = "Whoops!  Illegal value entered.  Try Again!")
+            
     
         elif cur_num == 3:
             #prompting current value
-            pass
+            try:
+                cur_targ = int(update.message.text)
+                
+                if cur_targ < 0:
+                    #illegal input, but doesn't raise error
+                    raise ValueError
+                    
+                #calc newnum
+                c.execute('''
+                          SELECT COUNT(*) 
+                          FROM resolutions 
+                          WHERE user_id=?
+                          ''', (user_id,))
+                
+                new_num = c.fetchone()[0]
+                
+                #update db
+                c.execute("""
+                          UPDATE resolutions
+                          SET current =?
+                          WHERE user_id=? 
+                          AND num = ?                      
+                          """, (cur_targ, user_id, new_num))
+                
+                conn.commit()
+                
+                #update status
+                c.execute("""
+                          UPDATE user_status
+                          SET num = 0,
+                          status = 'idle'
+                          WHERE chat_id=?
+                          """, (user_id,))
+                
+                conn.commit()
+                conn.close()
+                
+                #send response
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text = 'Perfect!  The Resolution has been added!')
+                update_percents(user_id)
+                
+            except ValueError:
+                #illegal input
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text = "Whoops!  Illegal value entered.  Try Again!")
     
-        
+        #completed to here but not tested
         
         pass
     elif cur_status == 'updating':
@@ -235,10 +414,6 @@ def input_loop(update, context):
         #no command currently running
     
 # # # ~ ~ ~ Other Functions ~ ~ ~ # # #
-def file_not_found(update, context):
-    """Display that tracking hasn't been started yet"""
-    pass
-
 def setup_db():
     """Check if db exists or create it if not"""
     #check if db exists
@@ -264,7 +439,31 @@ def setup_db():
         #table doesn't exist
         df = pd.DataFrame(columns=FILE_TEMPLATE)
         df.to_sql('resolutions', conn, index=False)
-        
+
+
+def update_percents(user_id):
+    """update the percent and pace for a given user_id"""
+    #TODO update percents
+    conn = sqlite3.connect('data.db')
+    
+    c = conn.cursor()
+    
+    cur_week_percent = datetime.date.isocalendar(datetime.date.today())[1] / 52
+    
+    #yearly totals
+    #TODO have not tested
+    c.execute("""
+              UPDATE resolutions
+              SET pace = ROUND((? * target)),
+              cur_percent = ROUND((current / target * 100), 0),
+              pace_percent = ROUND((? * 100), 0)
+              WHERE user_id = ?
+              AND type = "Yearly Total"
+              """, (cur_week_percent, cur_week_percent, user_id))
+    
+    #TODO one-offs
+    
+      
 # # # ~ ~ ~ Main ~ ~ ~ # # #
 def main():
     """Main Code execution"""
@@ -285,6 +484,12 @@ def main():
     
     start_tracking_handler = CommandHandler('newtrack', start_tracking)
     dispatcher.add_handler(start_tracking_handler)
+    
+    adding_handler = CommandHandler('add', add_reso)
+    dispatcher.add_handler(adding_handler)
+    
+    input_loop_handler = MessageHandler(Filters.text & (~Filters.command), input_loop)
+    dispatcher.add_handler(input_loop_handler)
     
     #start polling
     updater.start_polling()
